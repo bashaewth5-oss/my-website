4c9f123bb28d4d8262b4f05fbee657f67f28fb29
bashaewth5-oss <bashaewth5@gmail.com>
2026-09-14 17:18:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'محلل أنظمة | مدير قواعد بيانات | مطور أنظمة إدارية 🚀';
+});
